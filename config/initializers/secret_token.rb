4f46481c28eed834704b4f054234387a0ed1fa9f
Anthony Williams <hi@antw.me>
2011-09-22 13:56:14 +0100

    Wrapping.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Etflex::Application.config.secret_token = '208c672c7dc1eeaefc47531bf7c4a616811dd627cc669477788a78e0aaa06c335b782f0e651fcc9c856ce56e4de7681d9cb08fb494d887347bc77abf4ccf2b0c'
+Etflex::Application.config.secret_token =
+  '208c672c7dc1eeaefc47531bf7c4a616811dd627cc669477788a78e0aaa06c33' \
+  '5b782f0e651fcc9c856ce56e4de7681d9cb08fb494d887347bc77abf4ccf2b0c'
