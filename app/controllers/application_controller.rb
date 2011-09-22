622e7227b1b0475389f354234af5e497a2e7adf1
Anthony Williams <hi@antw.me>
2011-09-22 14:17:34 +0100

    Add a test action to verify that Haml works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # A temporary action used to verify that Haml, RSpec, and other dependencies
+  # are correctly configured.
+  #
+  # GET /sanity
+  #
+  def sanity
+  end
+
 end
