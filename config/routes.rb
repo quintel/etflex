7a3223e5a338d276077209ad3d5d6c08909f7db0
Anthony Williams <hi@antw.me>
2011-09-22 15:41:04 +0100

    Remove Rails default index and logo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ETF::Application.routes.draw do
   get 'sanity', to: 'application#sanity'
+  root          to: redirect('/sanity')
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
