503fbc86e291cadd32eb9d1816a7c2763b151d22
Anthony Williams <hi@antw.me>
2011-09-22 13:46:13 +0100

    Create a brand new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etflex::Application.config.secret_token = '208c672c7dc1eeaefc47531bf7c4a616811dd627cc669477788a78e0aaa06c335b782f0e651fcc9c856ce56e4de7681d9cb08fb494d887347bc77abf4ccf2b0c'
