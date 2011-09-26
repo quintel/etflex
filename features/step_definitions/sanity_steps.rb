893b6a770098da9c86b46761b1cf99d2cbfa5079
Anthony Williams <hi@antw.me>
2011-09-26 16:39:17 +0100

    Test JS pages with Selenium.

--- features/step_definitions/sanity_steps.rb
+++ features/step_definitions/sanity_steps.rb
@@ -1,3 +1,7 @@
-When /^I should see that the page is loading$/ do
-  Then %(I should see "Loading...")
+When /^I should see that the page loaded$/ do
+  Then %(I should see "It lives!")
+end
+
+When /^I should see the ETLite recreation$/ do
+  Then %(I should see "ETLite Recreation")
 end
