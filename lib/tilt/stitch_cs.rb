fcbeacb155202f5bdf97efc538a31f89b7a3dd78
Anthony Williams <hi@antw.me>
2011-10-30 17:02:20 +0000

    Only CS files in /client are CommonJS modules.

--- lib/tilt/stitch_cs.rb
+++ lib/tilt/stitch_cs.rb
@@ -1,35 +1,31 @@
 require 'tilt'
 
 module ETFlex::Tilt
-  class StitchCS < Tilt::Template
+  # A customised version of the CoffeeScript template which will handle all
+  # CoffeeScript normally, except those in /client which will be created as
+  # modules for use with Stitch.
+  #
+  class StitchCS < Tilt::CoffeeScriptTemplate
     self.default_mime_type = 'application/javascript'
 
-     def self.engine_initialized?
-      defined? ::CoffeeScript
-    end
-
-    def initialize_engine
-      require_template_library 'coffee_script'
-    end
-
-    def prepare
-      options[:bare] = true
-    end
-
+    # Compiles the CoffeeScript source.
+    #
+    # If the source file is in the /client directory, it will be compiled as a
+    # CommonJS module for use with Stitch.
+    #
     def evaluate(scope, locals, &block)
-      @output ||= <<-EOF.gsub(/^ {8}/, '')
-        require.define({ '#{ module_name(scope) }': function(exports, require, module) {
-          #{ CoffeeScript.compile(data, options) }
-        }});
-      EOF
+      if scope.root_path.split('/').last == 'client'
+        @output ||=
+          <<-EOF.gsub(/^ {12}/, '')
+            require.define({ '#{ scope.logical_path }': function(exports, require, module) {
+            #{ CoffeeScript.compile(data, bare: true) }
+            }});
+          EOF
+      else
+        # Not in /client, just compile a normal CoffeeScript.
+        super
+      end
     end
 
-    #######
-    private
-    #######
-
-    def module_name(scope)
-      scope.logical_path
-    end
-  end
+  end # StitchCS
 end # ETFlex::Tilt
