d75594df4a179ef3fdcdf9d96dca601b60734384
Anthony Williams <hi@antw.me>
2011-10-31 00:21:16 +0000

    Senario CRUD routing specs should be pending.

--- spec/routing/scenarios_routing_spec.rb
+++ spec/routing/scenarios_routing_spec.rb
@@ -1,34 +1,34 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe ScenariosController do
-  describe "routing" do
+describe 'ScenariosController' do
+  describe 'routing' do
 
-    it "routes to #index" do
-      get("/scenarios").should route_to("scenarios#index")
+    xit 'routes to #index' do
+      get('/scenarios').should route_to('scenarios#index')
     end
 
-    it "routes to #new" do
-      get("/scenarios/new").should route_to("scenarios#new")
+    xit 'routes to #new' do
+      get('/scenarios/new').should route_to('scenarios#new')
     end
 
-    it "routes to #show" do
-      get("/scenarios/1").should route_to("scenarios#show", :id => "1")
+    it 'routes to #show' do
+      get('/scenarios/1').should route_to('application#render_client', id: '1')
     end
 
-    it "routes to #edit" do
-      get("/scenarios/1/edit").should route_to("scenarios#edit", :id => "1")
+    xit 'routes to #edit' do
+      get('/scenarios/1/edit').should route_to('scenarios#edit', :id => '1')
     end
 
-    it "routes to #create" do
-      post("/scenarios").should route_to("scenarios#create")
+    xit 'routes to #create' do
+      post('/scenarios').should route_to('scenarios#create')
     end
 
-    it "routes to #update" do
-      put("/scenarios/1").should route_to("scenarios#update", :id => "1")
+    xit 'routes to #update' do
+      put('/scenarios/1').should route_to('scenarios#update', :id => '1')
     end
 
-    it "routes to #destroy" do
-      delete("/scenarios/1").should route_to("scenarios#destroy", :id => "1")
+    xit 'routes to #destroy' do
+      delete('/scenarios/1').should route_to('scenarios#destroy', :id => '1')
     end
 
   end
