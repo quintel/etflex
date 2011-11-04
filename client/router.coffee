quintel/etflex

commit d44627e7bdb2a1028e191d24dac7d47e83c56d60

Removed rotating animation for -webkit The rotation made the animation a bit too much for Chrome I leaved it as it is for Mozilla

client/router.coffee
@@ -36,7 +36,7 @@ class exports.Router extends Backbone.Router
   # GET /
   #
   root: ->
-    app.router.navigate 'sanity', true
+    app.router.navigate 'etlite', true
 
   # A test page which shows the all of the application dependencies are
   # correctly installed and work as intended.

app/assets/stylesheets/scenarios/_modern.sass
@@ -56,43 +56,9 @@
   from
     left: 1700px
     top: 30px
-    -webkit-transform: rotate(8deg)
-  20%
-    top: -10px
-    -webkit-transform: rotate(8deg)
-  25%
-    top: -16px
-    -webkit-transform: rotate(7deg)
-  30%
-    top: -22px
-    -webkit-transform: rotate(6deg)
-  35%
-    top: -27px
-    -webkit-transform: rotate(5deg)
-  40%
-    top: -30px
-    -webkit-transform: rotate(4deg)
-  50%
-    -webkit-transform: rotate(0deg)
-  60%
-    top: -30px
-    -webkit-transform: rotate(-4deg)
-  65%
-    top: -27px
-    -webkit-transform: rotate(-5deg)
-  70%
-    top: -22px
-    -webkit-transform: rotate(-6deg)
-  75%
-    top: -16px
-    -webkit-transform: rotate(-7deg)
-  80%
-    top: -10px
-    -webkit-transform: rotate(-8deg)
   to
     left: -400px
-    top: 50px
-    -webkit-transform: rotate(-10deg)
+    top: 30px
 
 // Styles --------------------------------------------------------------------
 
@@ -207,5 +173,5 @@
       .plane
         @include modern-prop(plane, 45px, 22px, 620px, -30px)
 
-        -moz-animation:    scenarioModernPlane 45s infinite linear
-        -webkit-animation: scenarioModernPlane 45s infinite linear
+        -moz-animation:    scenarioModernPlane 60s infinite linear
+        -webkit-animation: scenarioModernPlane 60s infinite linear

client/templates/scenarios/modern/header.eco
@@ -1,6 +1,7 @@
 <div id="theme-header">
   <a href="/" id="logo">Energie Transitie Model (lite)</a>
   <div class="world">
+    <div class="plane"></div>
     <div class="city"></div>
     <div class="turbines"></div>
     <div class="left-hills"></div>
@@ -9,6 +10,5 @@
     <div class="solar-panels"></div>
     <div class="electric-car"></div>
     <div class="sun"></div>
-    <div class="plane"></div>
   </div>
 </div>

commit 608af1e49a31ff48b9b11adcddb913fd8a4b922a

The "etlite" action is deprecated.
Fixes error introduced in [d44627e7] where the root action was changed
to direct to /etlite, when it should have been /scenarios/1

client/router.coffee
@@ -36,7 +36,7 @@ class exports.Router extends Backbone.Router
   # GET /
   #
   root: ->
-    app.router.navigate 'etlite', true
+    app.router.navigate '/scenarios/1', true
 
   # A test page which shows the all of the application dependencies are
   # correctly installed and work as intended.