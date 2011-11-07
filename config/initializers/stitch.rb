2c4e26861c5ca83ae8d490ba550d9507629f06e3
Anthony Williams <hi@antw.me>
2011-11-07 15:52:24 +0000

    Custom assets engines need to be available in production.
    Since Sprockets is now included in the production env also, this is
    perfectly OK, and fixes inclusion of Eco templates in the final
    application.js file.

--- config/initializers/stitch.rb
+++ config/initializers/stitch.rb
@@ -1,7 +1,5 @@
-if ETFlex::Application.config.assets.compile
-  require 'tilt/stitch_cs'
-  require 'tilt/stitch_eco'
+require 'tilt/stitch_cs'
+require 'tilt/stitch_eco'
 
-  ETFlex::Application.assets.register_engine '.coffee', ETFlex::Tilt::StitchCS
-  ETFlex::Application.assets.register_engine '.eco',    ETFlex::Tilt::StitchEco
-end
+ETFlex::Application.assets.register_engine '.coffee', ETFlex::Tilt::StitchCS
+ETFlex::Application.assets.register_engine '.eco',    ETFlex::Tilt::StitchEco
