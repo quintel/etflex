5de209e7a5064f7593b29c3f07286360ef8060f9
Anthony Williams <hi@antw.me>
2011-11-13 14:52:35 +0000

    Document deploy:link_config cap task.

--- lib/capistrano/link_config.rb
+++ lib/capistrano/link_config.rb
@@ -8,6 +8,12 @@ def link_config_command(file)
 end
 
 namespace :deploy do
+  desc <<-DESC
+    Creates symlinks to the configuration on the deployment server. Each \
+    server has it's own copy of the configuration files (database.yml, etc) \
+    which reside in the shared directory. This task creates a symlinks to \
+    each of the config files into the "current_path" config directory.
+  DESC
   task :link_config, roles: :app, except: { no_release: true } do
     run link_config_command('database.yml')
   end
