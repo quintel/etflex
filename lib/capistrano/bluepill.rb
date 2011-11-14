6e5f97ce3a0febdf24da6250886d96f4550682b8
Anthony Williams <hi@antw.me>
2011-11-14 19:16:15 +0000

    Add Bluepill Capistrano configuration.

---
+++ lib/capistrano/bluepill.rb
@@ -0,0 +1,48 @@
+# Runs a Bluepill command.
+#
+# @param [String] command The command to be sent to Bluepill.
+#
+def run_bluepill(command)
+  run "cd #{current_path} && bundle exec bluepill #{command} --no-privileged"
+end
+
+# Bluepill is used to monitor the Unicorn processes.
+namespace :bluepill do
+  desc <<-DESC
+    Loads Bluepill. Begins the processes it is expected to monitor (Unicorn) \
+    and relaunches them should they crash.
+  DESC
+  task :start, roles: :app do
+    # puts '*** Cannot manually start Bluepill; it is managed by Upstart.'
+    # puts '    See: /etc/init/bluepill.etflex.conf'
+
+    run_bluepill "load #{current_path}/config/bluepill/#{stage}.rb"
+  end
+
+  desc <<-DESC
+    Stops running Bluepill. Note that because Bluepill is an Upstart job, \
+    the process will be immediately restarted, as will all the monitored \
+    processes.
+  DESC
+  task :quit, roles: :app do
+    run_bluepill 'stop'
+    run_bluepill 'quit'
+  end
+
+  desc <<-DESC
+    Restarts Bluepill and reloads the configuration. Since Bluepill is an \
+    Upstart job, simply quitting Bluepill is sufficient since Upstart will \
+    bring it right back up again.
+  DESC
+  task :reload, roles: :app do
+    quit
+  end
+
+  desc <<-DESC
+    Shows the current Bluepill status. Outputs useful information on the \
+    health and status of the processes being monitored by Bluepill.
+  DESC
+  task :status, roles: :app do
+    run_bluepill 'status'
+  end
+end
