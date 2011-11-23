676cfc8ed7d1e2e351d9c6f2a4071d970119a970
Anthony Williams <hi@antw.me>
2011-11-23 20:37:25 +0000

    Migration clean-up, import data with rake db:seed

---
+++ db/migrate/20111123161847_create_basic_tables.rb
@@ -0,0 +1,28 @@
+class CreateBasicTables < ActiveRecord::Migration
+  def change
+    create_table :inputs do |t|
+      t.string  :key,                       null: false
+      t.float   :step,      default: 1.0,   null: false
+      t.float   :min,       default: 0.0,   null: false
+      t.float   :max,       default: 100.0, null: false
+      t.float   :start,     default: 0.0,   null: false
+      t.string  :unit
+      t.integer :remote_id,                 null: false
+    end
+
+    add_index :inputs, :remote_id, unique: true
+
+    create_table :scene_inputs do |t|
+      t.integer :scene_id,                  null: false
+      t.integer :input_id,                  null: false
+      t.boolean :placement, default: false, null: false
+      t.integer :position
+    end
+
+    add_index :scene_inputs, [ :scene_id, :input_id ]
+
+    create_table :scenes do |t|
+      t.string  :name
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# INPUTS ---------------------------------------------------------------------
+
+YAML.load_file(Rails.root.join('db/seeds/inputs.yml')).each do |data|
+  unless Input.create(data)
+    raise "Failed to save input: #{data['key']}, #{input.errors.inspect}"
+  end
+end
+
+# SCENES ---------------------------------------------------------------------
+
+YAML.load_file(Rails.root.join('db/seeds/scenes.yml')).each do |data|
+  scene = Scene.new name: data['name']
+
+  unless scene.save
+    raise "Failed to save scene: #{data['name']}, #{scene.errors.inspect}"
+  end
+
+  data['left_inputs'].each do |input|
+    scene.left_scene_inputs.create!(
+      input_id: Input.where(remote_id: input).first.id)
+  end
+
+  data['left_inputs'].each do |input|
+    scene.right_scene_inputs.create!(
+      input_id: Input.where(remote_id: input).first.id)
+  end
+end
