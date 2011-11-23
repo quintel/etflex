4df9520ba479affbd6babcb3ad5c4e391fb52f05
Anthony Williams <hi@antw.me>
2011-11-23 12:52:16 +0000

    Rename Blueprint to Scene, BlueprintInput to SceneInput.

--- spec/models/scene_input_spec.rb
+++ spec/models/scene_input_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe BlueprintInput do
+describe SceneInput do
 
   # RELATIONSHIPS ------------------------------------------------------------
 
-  it { should belong_to(:blueprint) }
+  it { should belong_to(:scene) }
   it { should belong_to(:input) }
 
-  # BLUEPRINT ID -------------------------------------------------------------
+  # SCENE ID -----------------------------------------------------------------
 
-  it { should validate_presence_of(:blueprint_id) }
+  it { should validate_presence_of(:scene_id) }
 
   # INPUT ID -----------------------------------------------------------------
 
@@ -24,21 +24,21 @@ describe BlueprintInput do
 
   context '#left?' do
     it 'should be true when placement=0' do
-      BlueprintInput.new(placement: false).should be_left
+      SceneInput.new(placement: false).should be_left
     end
 
     it 'should be false when placement=1' do
-      BlueprintInput.new(placement: true).should_not be_left
+      SceneInput.new(placement: true).should_not be_left
     end
   end
 
   context '#right?' do
     it 'should be false when placement=0' do
-      BlueprintInput.new(placement: false).should_not be_right
+      SceneInput.new(placement: false).should_not be_right
     end
 
     it 'should be true when placement=1' do
-      BlueprintInput.new(placement: true).should be_right
+      SceneInput.new(placement: true).should be_right
     end
   end
 
