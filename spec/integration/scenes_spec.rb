28fd7bf736d93d611cb8eb4d9902aff31df5e672
Anthony Williams <hi@antw.me>
2011-11-23 14:02:23 +0000

    Use /scenes in integration tests.

--- spec/integration/scenes_spec.rb
+++ spec/integration/scenes_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe 'Modules' do
+describe 'Scenes' do
 
   # --------------------------------------------------------------------------
 
-  context 'retrieving a module', api: true do
-    before  { visit '/modules/1' }
+  context 'retrieving a scene', api: true do
+    before  { visit '/scenes/1' }
     subject { JSON.parse page.source }
 
     it { page.status_code.should eql(200) }
