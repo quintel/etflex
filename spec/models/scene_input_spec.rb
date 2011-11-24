eb95dab2b9e3ba56dfa27d76ceb93fe2d0d5453b
Anthony Williams <hi@antw.me>
2011-11-24 13:31:23 +0000

    Allow SceneInput to customise the input min, max, and start values.

--- spec/models/scene_input_spec.rb
+++ spec/models/scene_input_spec.rb
@@ -31,4 +31,61 @@ describe SceneInput do
     it { should be_right }
   end
 
+  # MIN ----------------------------------------------------------------------
+
+  describe '#min' do
+    subject { SceneInput.new(input: Input.new(min: 50)) }
+
+    it 'should return the value when set' do
+      subject.min = 75
+      subject.min.should eql(75.0)
+    end
+
+    it 'should delegate to the input when no value is set' do
+      subject.min.should eql(50.0)
+    end
+
+    it 'should return nil if no value, and no input is set' do
+      SceneInput.new.min.should be_nil
+    end
+  end
+
+  # MAX ----------------------------------------------------------------------
+
+  describe '#max' do
+    subject { SceneInput.new(input: Input.new(max: 50)) }
+
+    it 'should return the value when set' do
+      subject.max = 75
+      subject.max.should eql(75.0)
+    end
+
+    it 'should delegate to the input when no value is set' do
+      subject.max.should eql(50.0)
+    end
+
+    it 'should return nil if no value, and no input is set' do
+      SceneInput.new.max.should be_nil
+    end
+  end
+
+  # START --------------------------------------------------------------------
+
+  describe '#start' do
+    subject { SceneInput.new(input: Input.new(start: 50)) }
+
+    it 'should return the value when set' do
+      subject.start = 75
+      subject.start.should eql(75.0)
+    end
+
+    it 'should delegate to the input when no value is set' do
+      subject.start.should eql(50.0)
+    end
+
+    it 'should return nil if no value, and no input is set' do
+      SceneInput.new.start.should be_nil
+    end
+  end
+
 end
