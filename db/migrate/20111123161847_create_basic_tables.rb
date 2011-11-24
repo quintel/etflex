bb82a52188eeaff56336e35edb6cef35ad453cb7
Anthony Williams <hi@antw.me>
2011-11-24 00:51:49 +0000

    Add min, max, and start to SceneInput.
    Will permit customising the min, max, and start value values of an input
    for each scenario.

--- db/migrate/20111123161847_create_basic_tables.rb
+++ db/migrate/20111123161847_create_basic_tables.rb
@@ -1,13 +1,13 @@
 class CreateBasicTables < ActiveRecord::Migration
   def change
     create_table :inputs do |t|
-      t.string  :key,                       null: false
-      t.float   :step,      default: 1.0,   null: false
-      t.float   :min,       default: 0.0,   null: false
-      t.float   :max,       default: 100.0, null: false
-      t.float   :start,     default: 0.0,   null: false
-      t.string  :unit
       t.integer :remote_id,                 null: false
+      t.string  :key,                       null: false
+      t.float   :step,      default:   1.0, null: false
+      t.float   :min,       default:   0.0, null: false
+      t.float   :max,       default: 100.0, null: false
+      t.float   :start,     default:   0.0, null: false
+      t.string  :unit
     end
 
     add_index :inputs, :remote_id, unique: true
@@ -17,6 +17,12 @@ class CreateBasicTables < ActiveRecord::Migration
       t.integer :input_id,                  null: false
       t.boolean :left,      default: true,  null: false
       t.integer :position
+
+      # Customised input values.
+
+      t.float   :min,       default:   0.0, null: false
+      t.float   :max,       default: 100.0, null: false
+      t.float   :start,     default:   0.0, null: false
     end
 
     add_index :scene_inputs, [ :scene_id, :input_id ]
