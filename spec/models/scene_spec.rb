4b4cfaa31d6f21e401362f98d61d5aac76ea9878
Anthony Williams <hi@antw.me>
2011-11-25 16:49:47 +0000

    Limit Scene#name to 100 characters.

--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -5,8 +5,8 @@ describe Scene do
   # RELATIONSHIPS ------------------------------------------------------------
 
   it { should have_many(:scene_inputs) }
-  it { should have_many(:left_scene_inputs).conditions(placement: false) }
-  it { should have_many(:right_scene_inputs).conditions(placement: true) }
+  it { should have_many(:left_scene_inputs).conditions(left: true) }
+  it { should have_many(:right_scene_inputs).conditions(left: false) }
 
   it { should have_many(:inputs).through(:scene_inputs) }
   it { should have_many(:left_inputs).through(:left_scene_inputs) }
@@ -15,5 +15,6 @@ describe Scene do
   # NAME ---------------------------------------------------------------------
 
   it { should validate_presence_of(:name) }
+  it { should ensure_length_of(:name).is_at_most(100) }
 
 end
