7417ff6f481dbdc432fab89e288daf4550ac19a4
Anthony Williams <hi@antw.me>
2011-11-25 13:57:43 +0000

    Aggregate inputs into a Scenes#inputs collection.

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -3,11 +3,13 @@ class Scene < ActiveRecord::Base
   # RELATIONSHIPS ------------------------------------------------------------
 
   with_options class_name: 'SceneInput' do |opts|
+    opts.has_many :scene_inputs
     opts.has_many :left_scene_inputs,  conditions: { left: true  }
     opts.has_many :right_scene_inputs, conditions: { left: false }
   end
 
   with_options class_name: 'Input', source: :input, readonly: true do |opts|
+    opts.has_many :inputs,       through: :scene_inputs
     opts.has_many :left_inputs,  through: :left_scene_inputs
     opts.has_many :right_inputs, through: :right_scene_inputs
   end
