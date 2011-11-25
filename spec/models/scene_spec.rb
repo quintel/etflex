b36ab3e856b5240f3b6b40cbfc871090f8a9e2d7
Anthony Williams <hi@antw.me>
2011-11-25 14:04:21 +0000

    Spec Scene#inputs and Scene#scene_inputs

--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -4,9 +4,11 @@ describe Scene do
 
   # RELATIONSHIPS ------------------------------------------------------------
 
+  it { should have_many(:scene_inputs) }
   it { should have_many(:left_scene_inputs).conditions(placement: false) }
   it { should have_many(:right_scene_inputs).conditions(placement: true) }
 
+  it { should have_many(:inputs).through(:scene_inputs) }
   it { should have_many(:left_inputs).through(:left_scene_inputs) }
   it { should have_many(:right_inputs).through(:right_scene_inputs) }
 
