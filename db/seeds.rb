662394e280364bfe59b8c8c41568520e0f5e9b5e
Anthony Williams <hi@antw.me>
2011-11-28 16:50:27 +0000

    Add seeding of inputs and scenes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts  'Seeding will remove EVERYTHING from the DB. Are you sure?'
+print 'Enter "yes" or "y" to continue; anything else will abort: '
+
+unless $stdin.gets.chomp =~ /^y(es)?$/
+  puts 'Aborted. Goodbye.'
+  exit 0
+end
+
+puts 'Removing old records...'
+Mongoid.database.collections.each { |collection| collection.remove }
+
+# INPUTS ---------------------------------------------------------------------
+
+puts 'Importing inputs...'
+
+YAML.load_file(Rails.root.join('db/seeds/inputs.yml')).each do |data|
+  unless Input.create(data)
+    raise "Failed to save input: #{data['key']}, #{input.errors.inspect}"
+  end
+end
+
+# SCENES ---------------------------------------------------------------------
+
+puts 'Importing scenes...'
+
+YAML.load_file(Rails.root.join('db/seeds/scenes.yml')).each do |data|
+  scene = Scene.new name: data['name']
+
+  unless scene.save
+    raise "Failed to save scene: #{data['name']}, #{scene.errors.inspect}"
+  end
+
+  data['left_inputs'].each do |input|
+    scene.scene_inputs.create!(
+      left:  true,
+      input: Input.where(remote_id: input).first)
+  end
+
+  data['right_inputs'].each do |input|
+    scene.scene_inputs.create!(
+      left:  false,
+      input: Input.where(remote_id: input).first)
+  end
+end
+
+# ----------------------------------------------------------------------------
+
+puts 'All done'
