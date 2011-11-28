c2d940e778289be1080fe388ae2c0e5d4ca80d1e
Anthony Williams <hi@antw.me>
2011-11-28 17:56:38 +0000

    Add Gauge prop.
    A Gauge is a prop which moves a needle left and right depending on a
    query value. The gauge needs to know the query whose value should be
    used, and the value represented at the far left, and far right, of
    the gauge.
    
    An example is the "supply too high" / "supply too low" gauge used on
    the ETlite recreation scene.

---
+++ app/models/props/gauge.rb
@@ -0,0 +1,25 @@
+module Props
+  # A Gauge is a prop which moves a needle left and right depending on a query
+  # value. The gauge needs to know the query whose value should be used, and
+  # the value represented at the far left, and far right, of the gauge.
+  #
+  # An example is the "supply too high" / "supply too low" gauge used on the 
+  # ETlite recreation scene.
+  #
+  class Gauge < Prop
+
+    # FIELDS -----------------------------------------------------------------
+
+    field :query_key, type: String
+
+    field :min,       type: Float
+    field :max,       type: Float
+
+    # VALIDATION -------------------------------------------------------------
+
+    validates :query_key, presence: true
+    validates :min,       presence: true, numericality: true
+    validates :max,       presence: true, numericality: true
+
+  end # Gauge
+end # Props
