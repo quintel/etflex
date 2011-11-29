9b008ca64d5162ccebaf85350175dc2fb31fa79b
Anthony Williams <hi@antw.me>
2011-11-29 18:36:55 +0000

    Add and test Gauge factory.

--- spec/factories/props.rb
+++ spec/factories/props.rb
@@ -8,4 +8,12 @@ FactoryGirl.define do
     left_extent  1000
     right_extent 1000
   end
+
+  factory :gauge, class: Props::Gauge do
+    name      'balance of supply and demand'
+    query_key 'balance_of_supply_and_demand'
+
+    min 0.4
+    max 1.6
+  end
 end
