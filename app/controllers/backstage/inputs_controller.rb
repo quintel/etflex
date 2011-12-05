a2c8b2e85f8e598587c0e552dcd3e5d8d55fef13
Anthony Williams <hi@antw.me>
2011-12-05 18:47:35 +0000

    Add an API for editing Inputs.

--- app/controllers/backstage/inputs_controller.rb
+++ app/controllers/backstage/inputs_controller.rb
@@ -1,7 +1,25 @@
 class Backstage::InputsController < ApplicationController
   include ETFlex::ClientController
 
-  # Actions ------------------------------------------------------------------
+  # FILTERS ------------------------------------------------------------------
+
+  #######
+  private
+  #######
+
+  before_filter :fetch_input, except: [ :index, :new, :create ]
+
+  # Retrieves the input specified in the params.
+  #
+  def fetch_input
+    @input = Input.find(params[:id])
+  end
+
+  # ACTIONS ------------------------------------------------------------------
+
+  ######
+  public
+  ######
 
   # Returns a JSON list of all inputs present in the database.
   #
@@ -9,6 +27,15 @@ class Backstage::InputsController < ApplicationController
   #
   def index
     @inputs = Input.all
+  end
+
+  # Updates an input with the supplied params.
+  #
+  # PUT /backstage/inputs/:id
+  #
+  def update
+    @input.update_attributes(params[:input])
+    respond_with @input
   end
 
 end
