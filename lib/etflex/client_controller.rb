1142bd39b04313ea60d3883ab5f2d6246f89c5b4
Anthony Williams <hi@antw.me>
2011-12-05 16:53:52 +0000

    Add ETFlex::ClientController.
    Used as a mixin for any Controller which needs to provide support for
    rendering the Backbone client.
    
    Requires that POST, PUT, and DELETE request use JSON and sets the
    controller to use the ClientResponder.

---
+++ lib/etflex/client_controller.rb
@@ -0,0 +1,59 @@
+module ETFlex
+  # Mixes in functionality common to controllers which render the Backbone
+  # client by default (which should be the vast majority of controllers in
+  # ETFlex.
+  #
+  module ClientController
+    extend ActiveSupport::Concern
+
+    # Sets the typical REST actions so that create, edit and destroy actions
+    # are JSON-only.
+    #
+    included do
+      before_filter :restrict_html_to_get
+
+      respond_to :json
+      respond_to :html, except: [ :create, :update, :destroy ]
+    end
+
+    # Used as a before_filter in ClientControllers and forcefully denies HTML
+    # requests from issuing anything except a GET. Everything else must use
+    # JSON.
+    #
+    def restrict_html_to_get
+      head 406 unless request.get? or request.format.json?
+    end
+
+    # Returns a hash which may be used in conjunction with `render` so that
+    # you may simply
+    #
+    #   render client
+    #
+    # Pass extra rendering options to `client` has a hash, like so:
+    #
+    #   render client(meaning: 42)
+    #
+    def client(options = nil)
+      if options.nil? then { template: 'application/client' } else
+        options.merge template: 'application/client'
+      end
+    end
+
+    module ClassMethods
+      # Use the ClientResponder so that the Backbone client is rendered for
+      # HTML requests.
+      #
+      def responder
+        ETFlex::ClientResponder
+      end
+
+      # Hide the public "client" method used with render client, and by the
+      # ClientResponder.
+      #
+      def hide_actions
+        super.push :client
+      end
+    end # ClassMethods
+
+  end # ClientController
+end # ETFlex
---
+++ spec/controllers/client_controller_spec.rb
@@ -0,0 +1,122 @@
+require 'spec_helper'
+
+describe ETFlex::ClientController do
+  controller(ApplicationController) do
+    include ETFlex::ClientController
+
+    def index ;   respond_with([]) ; end
+    def show ;    respond_with({}) ; end
+    def new ;     respond_with({}) ; end
+    def create ;  respond_with({}, location: '/') ; end
+    def edit ;    respond_with({}) ; end
+    def update ;  respond_with({}, location: '/') ; end
+    def destroy ; respond_with({}) ; end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'GET /' do
+    it 'should accept HTML' do
+      get :index
+      response.status.should eql(200)
+    end
+
+    it 'should accept JSON' do
+      get :index, format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'GET /:id' do
+    it 'should accept HTML' do
+      get :show, id: '1'
+      response.status.should eql(200)
+    end
+
+    it 'should accept JSON' do
+      get :show, id: '1', format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'GET /new' do
+    it 'should accept HTML' do
+      get :new
+      response.status.should eql(200)
+    end
+
+    it 'should accept JSON' do
+      get :new, format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'GET /edit' do
+    it 'should accept HTML' do
+      get :edit, id: '1'
+      response.status.should eql(200)
+    end
+
+    it 'should accept JSON' do
+      get :edit, id: '1', format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'POST /' do
+    it 'should not accept HTML' do
+      post :create
+      response.status.should eql(406)
+    end
+
+    it 'should accept JSON' do
+      post :create, format: :json
+      response.status.should eql(201)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'PUT /:id' do
+    it 'should not accept HTML' do
+      put :update, id: '1'
+      response.status.should eql(406)
+    end
+
+    it 'should accept JSON' do
+      put :update, id: '1', format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'DELETE /:id' do
+    it 'should not accept HTML' do
+      delete :destroy, id: '1'
+      response.status.should eql(406)
+    end
+
+    it 'should accept JSON' do
+      delete :destroy, id: '1', format: :json
+      response.status.should eql(200)
+    end
+  end
+
+  # --------------------------------------------------------------------------
+
+  describe 'client method' do
+    it 'should not be reachable' do
+      expect { get :client }.to raise_error(ActionController::RoutingError)
+    end
+  end
+
+end
