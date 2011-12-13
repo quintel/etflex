035ec3a2b01282d30b98363e40945ed9390c2c03
Anthony Williams <hi@antw.me>
2011-12-13 19:44:46 +0000

    Clean up old integration specs.

--- spec/integration/scenes_spec.rb
+++ spec/integration/scenes_spec.rb
@@ -2,6 +2,55 @@ require 'spec_helper'
 
 describe 'Scenes' do
 
+  # --------------------------------------------------------------------------
+
+  specify 'Viewing the ETlite scene', js: true do
+
+    # Set up records.
+
+    input_one = create :input, key: 'transport_cars_electric_share'
+    input_two = create :input, key: 'number_of_coal_conventional'
+
+    scene = create :scene
+
+    scene.scene_inputs.create! input: input_one, location: 'left'
+    scene.scene_inputs.create! input: input_two, location: 'right'
+
+    # Finally...!
+
+    visit "/scenes/#{ scene.id }"
+
+    # Low-energy lighting.
+
+    page.should have_css('.label', text: 'Electric cars')
+
+    # Test that the Low-energy lighting range value is 0
+    #
+    # TODO Find a nice way to extract this into a method. Perhaps an
+    #      RSpec matcher?
+    #
+    #      page.should have_range('Electric cars', value: 0)
+    #
+    page.all('.range').each do |range|
+      if range.has_css?('.label', text: 'Electric cars')
+        range.should have_css('.output', text: '0')
+      end
+    end
+
+    # Electric cars.
+
+    page.should have_css('.label', text: 'Coal-fired power plants')
+
+    page.all('.range').each do |range|
+      if range.has_css?('.label', text: 'Coal-fired power plants')
+        range.should have_css('.output', text: '0')
+      end
+    end
+
+  end
+
+  # --------------------------------------------------------------------------
+
   context 'retrieving a list of scenes', api: true do
     let(:json) { JSON.parse page.source }
 
