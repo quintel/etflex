984df9878ef6874f267bdbbe44ee282220053c22
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2011-12-13 22:16:40 +0100

    fixing my previous fix. left is now location again in the schema and scene.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111123161847) do
+ActiveRecord::Schema.define(:version => 20111213210435) do
 
   create_table "inputs", :force => true do |t|
     t.integer "remote_id",                    :null => false
@@ -26,14 +26,17 @@ ActiveRecord::Schema.define(:version => 20111123161847) do
   add_index "inputs", ["remote_id"], :name => "index_inputs_on_remote_id", :unique => true
 
   create_table "props", :force => true do |t|
+    t.string "key",        :limit => 100, :null => false
     t.string "client_key", :limit => 100, :null => false
+    t.string "hurdles"
   end
 
   create_table "scene_inputs", :force => true do |t|
-    t.integer "scene_id",                   :null => false
-    t.integer "input_id",                   :null => false
-    t.boolean "left",     :default => true, :null => false
+    t.integer "scene_id",                :null => false
+    t.integer "input_id",                :null => false
+    t.string  "location", :limit => 100, :null => false
     t.integer "position"
+    t.float   "step"
     t.float   "min"
     t.float   "max"
     t.float   "start"
@@ -46,6 +49,8 @@ ActiveRecord::Schema.define(:version => 20111123161847) do
     t.integer "prop_id",                :null => false
     t.string  "location", :limit => 50, :null => false
   end
+
+  add_index "scene_props", ["scene_id", "prop_id"], :name => "index_scene_props_on_scene_id_and_prop_id", :unique => true
 
   create_table "scenes", :force => true do |t|
     t.string "name",     :limit => 100
