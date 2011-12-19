58b2e3bfb97549f25eb809e356c3337fc08c1563
Anthony Williams <hi@antw.me>
2011-12-19 14:42:26 +0000

    Properly test the scene inputs JSON.

--- spec/integration/scenes_spec.rb
+++ spec/integration/scenes_spec.rb
@@ -76,7 +76,20 @@ describe 'Scenes' do
   # --------------------------------------------------------------------------
 
   context 'retrieving a scene', api: true do
-    let(:scene) { create :scene }
+    let(:scene)  {   create(:scene)                   }
+    let(:inputs) { [ create(:input), create(:input) ] }
+    let(:props)  { [ create(:prop),  create(:prop)  ] }
+
+    before do
+      # Create two inputs, with two scene inputs; two props, and two
+      # scene props.
+
+      scene.scene_inputs.create! input: inputs[0], location: 'left'
+      scene.scene_inputs.create! input: inputs[1], location: 'right'
+
+      #scene.scene_props.build prop: props[0], location: 'center'
+      #scene.scene_props.build prop: props[1], location: 'bottom'
+    end
 
     before  { visit "/scenes/#{scene.id}" }
     subject { JSON.parse page.source }
@@ -86,8 +99,56 @@ describe 'Scenes' do
     it { should have_key('id')        }
     it { should have_key('name')      }
     it { should have_key('inputs')    }
+    it { should have_key('props')     }
     it { should have_key('centerVis') }
     it { should have_key('mainVis')   }
+
+    its(['inputs']) { should have(2).members }
+    #its(['props'])  { should have(2).members }
+
+    context 'inputs' do
+      context 'the first input' do
+        let(:input_json) { subject['inputs'].first }
+        let(:input)      { inputs.first }
+
+        it 'should be present' do
+          input_json.should_not be_nil
+        end
+
+        it 'should set the remoteId' do
+          input_json['remoteId'].should eql(input.remote_id)
+        end
+
+        it 'should include the location' do
+          input_json['location'].should eql('left')
+        end
+
+        it 'should include the position' do
+          input_json['position'].should eql(1)
+        end
+      end # the first input
+
+      context 'the second input' do
+        let(:input_json) { subject['inputs'].last }
+        let(:input)      { inputs.last }
+
+        it 'should be present' do
+          input_json.should_not be_nil
+        end
+
+        it 'should set the remoteId' do
+          input_json['remoteId'].should eql(input.remote_id)
+        end
+
+        it 'should include the location' do
+          input_json['location'].should eql('right')
+        end
+
+        it 'should include the position' do
+          input_json['position'].should eql(1)
+        end
+      end # the first input
+    end # inputs
   end
 
   # --------------------------------------------------------------------------
