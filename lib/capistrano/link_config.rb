22109116d00de27fc0f729cbd71c73b0635f48ba
Anthony Williams <hi@antw.me>
2011-12-19 22:40:16 +0000

    Copy database.yml, not mongoid.yml.

--- lib/capistrano/link_config.rb
+++ lib/capistrano/link_config.rb
@@ -10,11 +10,11 @@ end
 namespace :deploy do
   desc <<-DESC
     Creates symlinks to the configuration on the deployment server. Each \
-    server has it's own copy of the configuration files (mongoid.yml, etc) \
+    server has it's own copy of the configuration files (database.yml, etc) \
     which reside in the shared directory. This task creates a symlinks to \
     each of the config files into the "current_path" config directory.
   DESC
   task :link_config, roles: :app, except: { no_release: true } do
-    run link_config_command('mongoid.yml')
+    run link_config_command('database.yml')
   end
 end
