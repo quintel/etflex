584c6fc58e57efd1a79273316d77621370f11c4f
Anthony Williams <hi@antw.me>
2012-01-23 13:05:33 +0000

    Always use the session ID when using Scenarios.
    Changes the Scenario coffee class so that instead of saving the
    sessionID to the @id field, it instead uses @sessionId. Prevents
    Backbone from thinking that the Scenario is not @isNew() when in fact
    the scenario *is* new.
    
    Adjusts the routing so that /scene/:id/with/... can be used with the
    Backbone models when saving sceanrios to the server.

--- spec/routing/scenarios_routing_spec.rb
+++ spec/routing/scenarios_routing_spec.rb
@@ -4,38 +4,34 @@ describe 'ScenariosController' do
   describe 'routing' do
 
     it 'does not route to scenarios at the top level' do
-      get('/scenarios/1').should_not be_routable
+      get('/with/1').should_not be_routable
     end
 
     it 'does not route to #index' do
-      get('/scenes/1337/scenarios').should_not be_routable
+      get('/scenes/1337/with').should_not be_routable
     end
 
-    it 'routes to #show using /with/...' do
+    it 'routes to #show' do
       get('/scenes/1337/with/1').should \
         route_to('scenarios#show', scene_id: '1337', id: '1')
     end
 
-    it 'routes to #show using /scenarios/...' do
-      get('/scenes/1337/scenarios/1').should \
-        route_to('scenarios#show', scene_id: '1337', id: '1')
-    end
-
     it 'does not route to #edit' do
-      get('/scenes/1337/scenarios/1/edit').should_not be_routable
+      get('/scenes/1337/with/1/edit').should_not be_routable
     end
 
     it 'routes to #create' do
-      post('/scenes/1337/scenarios').should \
+      post('/scenes/1337/with').should \
         route_to('scenarios#create', scene_id: '1337')
     end
 
-    it 'does not route to #update' do
-      put('/scenes/1337/scenarios/1').should_not be_routable
+    it 'routes to #update' do
+      put('/scenes/1337/with/1').should \
+        route_to('scenarios#update', scene_id: '1337', id: '1')
     end
 
     it 'does not route to #destroy' do
-      delete('/scenes/1337/scenarios/1').should_not be_routable
+      delete('/scenes/1337/with/1').should_not be_routable
     end
 
   end
