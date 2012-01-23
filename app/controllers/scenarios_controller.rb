0230e5b629411fb387c4333a4195ffee9240acee
Anthony Williams <hi@antw.me>
2012-01-23 15:27:03 +0000

    Server: Support updating scenarios.
    Merged create with update, since the Scenarios controller behaves a
    little unusually; if the scenario exists it will be updated. Otherwise a
    new scenario will be created.

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -8,10 +8,13 @@ class ScenariosController < ApplicationController
   private
   #######
 
-  def new_scenario_attrs
+  def scenario_attrs
+    scenario = params.fetch(:scenario, Hash.new)
+
     { user:       current_user,
       scene:      Scene.find(params[:scene_id]),
-      session_id: params[:id] }
+      session_id: params[:id],
+      title:      scenario[:title] }
   end
 
   # ACTIONS ------------------------------------------------------------------
@@ -24,24 +27,33 @@ class ScenariosController < ApplicationController
   # scenario embedded within so that they client loads a specific ET-Engine
   # session.
   #
-  # GET /scenes/:scene_id/scenarios/:id
+  # GET /scenes/:scene_id/with/:id
   #
   def show
     @scenario   = Scenario.for_session *params.values_at(:scene_id, :id)
-    @scenario ||= Scenario.new new_scenario_attrs
+    @scenario ||= Scenario.new scenario_attrs
 
     respond_with @scenario
   end
 
-  # Creates a new Scenario record.
+  # Creates or updates a Scenario record.
   #
   # The client hits this action when it creates a new ET-Engine session so
   # that we may resume the users sessions later.
   #
-  # POST /scenes/:scene_id/scenarios
+  # PUT /scenes/:scene_id/with/:id
   #
-  def create
-    @scenario = Scenario.create new_scenario_attrs
+  def update
+    @scenario   = Scenario.for_session *params.values_at(:scene_id, :id)
+    @scenario ||= Scenario.new
+
+    if @scenario.persisted? and @scenario.user_id != current_user.id
+      return head :forbidden
+    end
+
+    @scenario.attributes = scenario_attrs
+    @scenario.save
+
     respond_with [ :scene, @scenario ]
   end
 
