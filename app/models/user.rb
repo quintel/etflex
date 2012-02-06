053912b2e12968f5d368b2622970b0c8feb92afc
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-02-06 11:29:18 +0000

    deleted hash example from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,42 +14,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :remember_me,
                   :password, :password_confirmation
 
-  # Creates a new user based on an AuthHash given by OmniAuth after
-  # authenticating the user with facebook. Will not create a user if the
-  # e-mail matches an existing user.
-  #
-  # The OmniAuth has looks like:
-  #
-  #   provider:          facebook
-  #   uid:              '1252568565'
-  #   info:
-  #     nickname:        dennis.schoenmakers
-  #     email:           dennis.schoenmakers@gmail.com
-  #     name:            Dennis Schoenmakers
-  #     first_name:      Dennis
-  #     last_name:       Schoenmakers
-  #     image:           http://graph.facebook.com/1252568565/picture
-  #     urls:
-  #       Facebook:      http://www.facebook.com/dennis.schoenmakers
-  #   credentials:
-  #     token:           AAAEYKztlzkgBAPWK02pRKXo8AyKouhML9I99kzV91Rzbrg \
-  #                      x8mX7EUfElOjBdXAmabax8kQIoIX9AzG5NhZCEaAFV5ymkZD
-  #     expires:         false
-  #   extra:
-  #     raw_info:
-  #       id:           '1252568565'
-  #       name:          Dennis Schoenmakers
-  #       first_name:    Dennis
-  #       last_name:     Schoenmakers
-  #       link:          http://www.facebook.com/dennis.schoenmakers
-  #       username:      dennis.schoenmakers
-  #       gender:        male
-  #       email:         dennis.schoenmakers@gmail.com
-  #       timezone:      1
-  #       locale:        nl_NL
-  #       verified:      true
-  #       updated_time: '2011-09-24T11:36:17+0000'
-  #
   def self.find_or_create_with_facebook!(auth_hash)
     if user = User.find_by_email(auth_hash.info.email) then user else
       User.create! do |user|
