fc2429c981257321f50ef2ae502af16da94bef99
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-02-06 16:29:02 +0000

    typo in parsed_response from geo-service, and making string nice for `United kingdom`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < Devise::RegistrationsController
     params[:user][:ip] = remote_ip
 
     request = HTTParty.get("http://api.ipinfodb.com/v3/ip-city/?key=1d0a3bc50d937dc0d04baa2af8bf4e6e6f594a1c27d0003d3634be881d5459ed&ip=#{remote_ip}&format=json")
-    params[:user][:city] = request.parsed_response[:countryName]
-    params[:user][:country] = request.parsed_response[:cityName]
-    params[:user][:latitude] = request.parsed_response[:latitude].to_f
-    params[:user][:longitude] = request.parsed_response[:longitude].to_f
+    params[:user][:city] = request.parsed_response['countryName'].titleize
+    params[:user][:country] = request.parsed_response['cityName'].titleize
+    params[:user][:latitude] = request.parsed_response['latitude'].to_f
+    params[:user][:longitude] = request.parsed_response['longitude'].to_f
 
     super
   end
