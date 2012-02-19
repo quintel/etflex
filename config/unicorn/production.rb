2e0c4292ff2b09b04f5681c890ae4d04a6266bda
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-02-19 15:24:05 +0100

    putting back /unicorn/production.rb so now we can savely deploy again

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,7 +2,7 @@
 app_directory = File.expand_path('../../../..', __FILE__)
 
 worker_processes 4
-working_directory "#{app_directory}/current"
+working_directory '/u/apps/etflex/current'
 
 # This loads the application in the master process before forking worker
 # processes. Read more about it here:
@@ -15,17 +15,17 @@ timeout 30
 
 # This is where we specify the socket. We will point the upstream Nginx module
 # to this socket later on
-listen "#{app_directory}/shared/pids/unicorn.sock", backlog: 64
+listen '/u/apps/etflex/shared/pids/unicorn.sock', backlog: 64
 
 # File containing the Unicorn process ID.
-pid "#{app_directory}/shared/pids/unicorn.pid"
+pid '/u/apps/etflex/shared/pids/unicorn.pid'
 
 # Set the path of the log files inside the log folder of the testapp
-stderr_path "#{app_directory}/shared/log/unicorn.log"
-stdout_path "#{app_directory}/shared/log/unicorn.log"
+stderr_path '/u/apps/etflex/shared/log/unicorn.log'
+stdout_path '/u/apps/etflex/shared/log/unicorn.log'
 
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = "#{app_directory}/current/Gemfile"
+  ENV['BUNDLE_GEMFILE'] = '/u/apps/etflex/current/Gemfile'
 end
 
 before_fork do |server, worker|
@@ -37,7 +37,7 @@ before_fork do |server, worker|
   # around until it is explicitly killed (so that it can be used if the new
   # master fails to start). Since we got as far as starting a new worker, we
   # end the old process...
-  old_pid = "#{app_directory}/shared/pids/unicorn.pid.oldbin"
+  old_pid = '/u/apps/etflex/shared/pids/unicorn.pid.oldbin'
 
   if File.exists?(old_pid) && server.pid != old_pid
     begin
