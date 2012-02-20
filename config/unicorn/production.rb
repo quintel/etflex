936e8e1a970b873a34479ac257dce65102437fbe
Anthony Williams <hi@antw.me>
2012-02-20 16:03:19 +0000

    Trying the relative-path Unicorn config again.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,8 +1,8 @@
 # Current file lives in /u/apps/:app_name/releases/:timestamp/config/unicorn/production.rb
-app_directory = File.expand_path('../../../..', __FILE__)
+app_dir = File.expand_path('../../..', File.dirname(__FILE__))
 
 worker_processes 4
-working_directory '/u/apps/etflex/current'
+working_directory "#{app_dir}/current"
 
 # This loads the application in the master process before forking worker
 # processes. Read more about it here:
@@ -15,17 +15,17 @@ timeout 30
 
 # This is where we specify the socket. We will point the upstream Nginx module
 # to this socket later on
-listen '/u/apps/etflex/shared/pids/unicorn.sock', backlog: 64
+listen "#{app_dir}/shared/pids/unicorn.sock", backlog: 64
 
 # File containing the Unicorn process ID.
-pid '/u/apps/etflex/shared/pids/unicorn.pid'
+pid "#{app_dir}/shared/pids/unicorn.pid"
 
 # Set the path of the log files inside the log folder of the testapp
-stderr_path '/u/apps/etflex/shared/log/unicorn.log'
-stdout_path '/u/apps/etflex/shared/log/unicorn.log'
+stderr_path "#{app_dir}/shared/log/unicorn.log"
+stdout_path "#{app_dir}/shared/log/unicorn.log"
 
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = '/u/apps/etflex/current/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/current/Gemfile"
 end
 
 before_fork do |server, worker|
@@ -37,7 +37,7 @@ before_fork do |server, worker|
   # around until it is explicitly killed (so that it can be used if the new
   # master fails to start). Since we got as far as starting a new worker, we
   # end the old process...
-  old_pid = '/u/apps/etflex/shared/pids/unicorn.pid.oldbin'
+  old_pid = "#{app_dir}/shared/pids/unicorn.pid.oldbin"
 
   if File.exists?(old_pid) && server.pid != old_pid
     begin
@@ -46,7 +46,6 @@ before_fork do |server, worker|
       # Old master already dead. Just ignore it.
     end
   end
-
 end
 
 after_fork do |server, worker|
