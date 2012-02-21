quintel/etflex

commit dfa87d11a5c8a2cd2e7429147df506eb8b05d25d

adding `badge` to view when server is on beta

client/models/scenario.coffee
@@ -47,7 +47,7 @@ class exports.Scenario extends Backbone.Model
       for query in queries.models
         queryResults[ query.id ] = query.get('future')
 
-      @set { queryResults }
+      set { queryResults }
 
     if inputs?.length
       for input in inputs.models

app/assets/stylesheets/_badge.sass
@@ -1,0 +1,30 @@
+// Taken from http://www.hagenburger.net/BLOG/3d-Github-badge-with-pure-CSS3.html
+#badge
+  left: -65px
+  height: 30px
+  top: 40px
+  position: absolute
+  text-decoration: none
+  width: 250px
+  @include rotate(-45deg)
+  z-index: 12
+  span
+    background: black
+    color: white
+    font-size: 14px
+    left: 0
+    padding: 5px 0
+    position: absolute
+    text-align: center
+    width: 250px
+    @include box-shadow(rgba(black, 0.2) 1px 1px 10px)
+    @include transition(opacity, 1s)
+    &:last-child
+      background: red
+      opacity: 0
+
+#badge:hover span
+  &:first-child
+    opacity: 0
+  &:last-child
+    opacity: 1

app/assets/stylesheets/application.css.sass
@@ -16,6 +16,9 @@
 @import "compass/css3/background-size"
 @import "compass/css3/border-radius"
 @import "compass/css3/images"
+@import "compass/css3/transform"
+@import "compass/css3/box-shadow"
+@import "compass/css3/transition"
 
 // Application Stylesheets ---------------------------------------------------
 
@@ -25,6 +28,7 @@
 @import "static-message"
 @import "buttons"
 @import "sign-in"
+@import "badge"
 
 // Module Stylesheets --------------------------------------------------------
 

client/app.coffee
@@ -95,3 +95,6 @@ commonBoot = (window, locale, user) ->
 installConsolePolyfill = (window) ->
   unless 'console' of window
     window.console = { log: (->), info: (->), warn: (->), error: (->) }
+    
+exports.isBeta = () ->
+  window.location.hostname.match(/beta/)?

client/templates/badge.eco
@@ -1,0 +1,4 @@
+<a id="badge" href="https://etflex.et-model.com">
+  <span>This is the Beta version!</span>
+  <span>Go to the 'real' live version</span>
+</a>

client/views/scene.coffee
@@ -3,6 +3,7 @@ app                = require 'app'
 api                = require 'lib/api'
 template           = require 'templates/scene'
 scenarioTempl      = require 'templates/scenario'
+badgeTempl         = require 'templates/badge'
 
 { RangeView }      = require 'views/range'
 { SceneNav }       = require 'views/scene_nav'
@@ -42,6 +43,7 @@ class exports.SceneView extends Backbone.View
 
   postRender: ->
     @renderInputs()
+    @renderBadge() if app.isBeta()
 
   # When a user tries to alter inputs on a scenario which isn't theirs, a "not
   # authorized" modal dialog appears allowing them to take suitable action.
@@ -173,7 +175,7 @@ class exports.SceneView extends Backbone.View
   #
   renderNavigation: ->
     @$('#footer').before (new SceneNav model: @model).render().el
-
+    
   # Creates the "Loading..." box which pops up at the bottom-left of the
   # scene view whenever an XHR request is pending.
   #
@@ -182,6 +184,9 @@ class exports.SceneView extends Backbone.View
 
     loader.ajaxStart -> loader.stop().animate bottom:   '0px', 'fast'
     loader.ajaxStop  -> loader.stop().animate bottom: '-36px', 'fast'
+
+  renderBadge: ->
+    $('body').append badgeTempl()
 
   # Initializes the scenarios list which appears on the right-hand side of the
   # scene and links to scenarios belonging to other users.

commit 28f439806c1c32101d9091dd5f6dc49eb7ee4533

Fix Scenario broken in dfa87d11.

client/models/scenario.coffee
@@ -47,7 +47,7 @@ class exports.Scenario extends Backbone.Model
       for query in queries.models
         queryResults[ query.id ] = query.get('future')
 
-      set { queryResults }
+      @set { queryResults }
 
     if inputs?.length
       for input in inputs.models