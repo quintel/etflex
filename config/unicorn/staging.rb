77ad0f0aa01bb2d12638492b0717252d6b2a325b
Anthony Williams <hi@antw.me>
2012-02-21 13:54:45 +0000

    Cannot "require" in a Unicorn config. Read+instance_eval instead.

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.dirname(__FILE__)) + '/production'
+prod = File.read(File.expand_path(File.dirname(__FILE__)) + '/production.rb')
+instance_eval(prod, __FILE__, __LINE__ + 1)
 
 # We require fewer workers in the staging environment.
 worker_processes 2
