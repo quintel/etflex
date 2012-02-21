66b53abdd44f6ca18920ef2e1d039a94ac36352b
Anthony Williams <hi@antw.me>
2012-02-21 14:18:05 +0000

    Clean up Unicorn staging config.

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,5 +1,9 @@
-prod = File.read(File.expand_path(File.dirname(__FILE__)) + '/production.rb')
-instance_eval(prod, __FILE__, __LINE__ + 1)
+production_path = File.expand_path(File.dirname(__FILE__)) + '/production.rb')
+production_conf = File.read(production_path)
+
+# Load the production configuration which will form the basis of the staging
+# environment. Staging-specific settings may be placed beneath this line.
+instance_eval production_conf, production_path
 
 # We require fewer workers in the staging environment.
 worker_processes 2
