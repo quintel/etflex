bc8b8afff5c82909cf34af2c8a1ef1945566934f
Anthony Williams <hi@antw.me>
2012-02-23 13:32:59 +0000

    Extract guest helpers from AppController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include ETFlex::LocaleController
+  include ETFlex::GuestController
+
+  layout :layout_by_resource
 
   # RESCUES ------------------------------------------------------------------
 
@@ -12,48 +15,11 @@ class ApplicationController < ActionController::Base
 
   # FILTERS ------------------------------------------------------------------
 
-  #######
-  private
-  #######
-
-  layout :layout_by_resource
-
-  before_filter :handle_guest_id
-
-  helper_method :guest_user
-  helper_method :current_or_guest_user
-
-  # Guests have a unique ID set so that they may save their scenarios.
-  #
-  def handle_guest_id
-    if user_signed_in? and cookies[:guest_id]
-      cookies.delete :guest_id
-    elsif ! user_signed_in? and cookies[:guest_id].blank?
-      cookies.permanent.signed[:guest_id] = {
-        httponly: true, value: SecureRandom.uuid }
-    end
-  end
-
-  # Returns the Guest instance for the current user. Will return nil if the
-  # user is signed into a registered account.
-  #
-  def guest_user
-    @_guest ||= Guest.new cookies.signed[:guest_id] unless user_signed_in?
-  end
-
-  # Returns the currently signed in user, or the Guest instance if the user
-  # is not signed in.
-  #
-  def current_or_guest_user
-    if user_signed_in? then current_user else guest_user end
-  end
-
   ##########
   protected
   ##########
 
   # Set the layout to Backstage's for all Devise views
-  #
   def layout_by_resource
     devise_controller? ? 'backstage' : 'application'
   end
