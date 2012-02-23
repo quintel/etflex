06a7f3925513b096cbcf28e05f2e88666b7ff0cf
Anthony Williams <hi@antw.me>
2012-02-23 16:01:15 +0000

    (Temporarily) send Ruby version with responses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include ETFlex::LocaleController
   include ETFlex::GuestController
 
+  before_filter :set_ruby_version_header
   layout :layout_by_resource
 
   # RESCUES ------------------------------------------------------------------
@@ -22,6 +23,19 @@ class ApplicationController < ActionController::Base
   # Set the layout to Backstage's for all Devise views
   def layout_by_resource
     devise_controller? ? 'backstage' : 'application'
+  end
+
+  # A temporary filter which sends the full Ruby version as an HTTP header.
+  # I'm using this to test the Ruby 1.9.3-p125 update and it will be removed
+  # again shortly.
+  #
+  def set_ruby_version_header
+    require 'rbconfig'
+
+    headers['X-Ruby-Version'] =
+      %w( RUBY_PROGRAM_VERSION PATCHLEVEL ).map do |key|
+        RbConfig::CONFIG[key]
+      end.join('-p')
   end
 
 end
