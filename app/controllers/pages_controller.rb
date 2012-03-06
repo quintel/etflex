1054d66c09b962f507b7403b9942e3cca01f4c71
Anthony Williams <hi@antw.me>
2012-03-06 00:43:47 +0000

    Add the improved high score table to the root page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,15 @@ class PagesController < ApplicationController
   #
   def root
     @scenes = Scene.limit(10)
+
+    # We need to select twice as many scenarios as are actually displayed; if
+    # a scenario currently in the top five is demoted, we need the next
+    # highest so that it can be promoted in the UI. So, twice as many allows
+    # all of the top five to be demoted without the UI crapping out.
+    #
+    # In the real world, (number_shown) + 2 should be enough...
+    #
+    @high_scenarios = Scenario.by_score.limit(10)
   end
 
   # A test page for Pusher.
