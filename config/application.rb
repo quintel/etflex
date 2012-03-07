9ad1fa0cbd1751ef37da249562d933b83f5c02ac
Anthony Williams <hi@antw.me>
2012-03-07 17:00:18 +0000

    Move the changing header into CoffeeScript.
    High scores are rendered with CS, might as well use it to make the
    changing header also. :)

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,6 @@ module ETFlex
     config.assets.precompile.push('backstage.css')
     config.assets.precompile.push('pages.css')
     config.assets.precompile.push('backstage.js')
-    config.assets.precompile.push('pages.js')
 
     # Version of your assets, change this if you want to expire all your
     # assets.
