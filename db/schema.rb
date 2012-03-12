23e3c227caefd94945f42fa2083fa5fc0a14541f
Anthony Williams <hi@antw.me>
2012-03-12 14:43:55 +0000

    Update input start values. Closes #140.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120227163049) do
+ActiveRecord::Schema.define(:version => 20120312141059) do
 
   create_table "inputs", :force => true do |t|
     t.integer "remote_id",                    :null => false
