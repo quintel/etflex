b0a4ed597b4c3deaad76dd4f92df0da4f845e6b5
Anthony Williams <hi@antw.me>
2012-04-25 15:15:23 +0100

    Recover from failed location lookup when offline. Closes #189.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,37 @@
 class UsersController < Devise::RegistrationsController
   def create
-    remote_ip = request.remote_ip
-    params[:user][:ip] = remote_ip
-
-    request = HTTParty.get("http://api.ipinfodb.com/v3/ip-city/?key=1d0a3bc50d937dc0d04baa2af8bf4e6e6f594a1c27d0003d3634be881d5459ed&ip=#{remote_ip}&format=json")
-    params[:user][:city] = request.parsed_response['countryName'].titleize
-    params[:user][:country] = request.parsed_response['cityName'].titleize
-    params[:user][:latitude] = request.parsed_response['latitude'].to_f
-    params[:user][:longitude] = request.parsed_response['longitude'].to_f
+    params[:user][:ip] = request.remote_ip
+    params[:user].merge request_location(request)
 
     super
+  end
+
+  #######
+  private
+  #######
+
+  # Location information about the given request.
+  #
+  # request - The request whose location is to be determined.
+  #
+  # Returns a hash of location information, or an empty hash if no location
+  # could be determined.
+  def request_location(req)
+    location_response = RestClient.get(
+      'http://api.ipinfodb.com/v3/ip-city/', params: {
+        key:    '1d0a3bc50d937dc0d04baa2af8bf4e6e' \
+                '6f594a1c27d0003d3634be881d5459ed',
+        ip:      req.remote_ip,
+        format: 'json' })
+
+    parsed = JSON.parse(location_response)
+
+    { country:   parsed['countryName'].titleize,
+      city:      parsed['cityName'].titleize,
+      latitude:  parsed['latitude'].to_f,
+      longitude: parsed['longitude'].to_f }
+
+  rescue SocketError, JSON::ParserError
+    Hash.new
   end
 end
