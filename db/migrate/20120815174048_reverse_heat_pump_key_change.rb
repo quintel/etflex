b6a254ebe92c3670df1793fce4ae2bd59e60baf2
Anthony Williams <hi@antw.me>
2012-08-15 18:43:26 +0100

    Reverse heat pump input key change. Reopens #205.
    The "households_space_heater_heatpump_ground_water_electricity" input
    does not (yet?) exist in ETsource.

---
+++ db/migrate/20120815174048_reverse_heat_pump_key_change.rb
@@ -0,0 +1,13 @@
+class ReverseHeatPumpKeyChange < ActiveRecord::Migration
+  def up
+    input = Input.where(key: 'households_space_heater_heatpump_ground_water_electricity').first!
+    input.key = 'households_heating_heat_pump_ground_share'
+    input.save!
+  end
+
+  def down
+    input = Input.where(key: 'households_heating_heat_pump_ground_share').first!
+    input.key = 'households_space_heater_heatpump_ground_water_electricity'
+    input.save!
+  end
+end
