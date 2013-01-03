7867672d7f33a1dbbd1590299fccc2ff5565e438
Anthony Williams <hi@antw.me>
2013-01-03 18:29:59 +0000

    Update all inputs to the latest ETsource values.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130103162002) do
+ActiveRecord::Schema.define(:version => 20130103181532) do
 
   create_table "inputs", :force => true do |t|
     t.string "key",                      :null => false
