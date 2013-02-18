02b7ce7867e7792ead259cb5f10fb6fcfae6b299
Paolo Zaccagnini <paozac@gmail.com>
2013-02-18 13:46:59 +0100

    Escape JS object

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def rtl
     @data = request.env['omniauth.auth']
-    @email = 'foobar@foo.com'
+    @email = @data['info']['email'] rescue nil
     render 'devise/rtl'
     # render :text => request.env['omniauth.auth'].to_yaml
   #   @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
