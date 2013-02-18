32342ecc2a5154ea3b108b90c31087b638ee0071
Paolo Zaccagnini <paozac@gmail.com>
2013-02-18 10:11:04 +0100

    Added omniauth-openid gem

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,14 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+  def rtl
+    @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
+
+    sign_in @user
+    redirect_to root_path, notice: "succesfully signed in!"
+  rescue ActiveRecord::RecordInvalid
+    raise request.env['omniauth.auth']
+  end
+
   def facebook
     @user = User.find_or_create_with_facebook!(request.env['omniauth.auth'])
 
