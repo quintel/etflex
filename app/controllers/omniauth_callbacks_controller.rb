a94c5a26b19eb1868044af1c74a680a0259819b3
Paolo Zaccagnini <paozac@gmail.com>
2013-02-19 12:08:54 +0100

    The name attribute isn't coming back

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def rtl
-    # @email = request.env['omniauth.auth']['info']['email'] rescue nil
-    # session[:guest_email] = @email
-    # session[:guest_name]  = @email.split('@').first # Any better idea?
-    # redirect_to '/scenes/2'
-    #
     @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
     sign_in @user
     redirect_to root_path, notice: "succesfully signed in!"
