f365d2a594bdcccab5fb9ec26c118f5c5f841387
Paolo Zaccagnini <paozac@gmail.com>
2013-02-20 10:33:42 +0100

    Store aside the current scenario before logging in

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def rtl
+    active = Scenario.for_user(current_or_guest_user).recent.first
     @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
     sign_in @user
 
-    if active = Scenario.for_user(current_or_guest_user).recent.first
+    if active
       redirect_to scene_scenario_path(scene_id: active.scene_id, id: active.id)
     else
       redirect_to root_path, notice: "succesfully signed in!"
