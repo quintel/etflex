3d0f9d2d45fafadbaf07898b79fc7c47a4dee506
Paolo Zaccagnini <paozac@gmail.com>
2013-02-20 10:24:24 +0100

    RTL Auth: redirect to current scenario if available

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def rtl
     @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
     sign_in @user
-    redirect_to root_path, notice: "succesfully signed in!"
+
+    if active = Scenario.for_user(current_or_guest_user).recent.first
+      redirect_to scene_scenario_path(scene_id: active.scene_id, id: active.id)
+    else
+      redirect_to root_path, notice: "succesfully signed in!"
+    end
   rescue ActiveRecord::RecordInvalid
     raise request.env['omniauth.auth']
   end
