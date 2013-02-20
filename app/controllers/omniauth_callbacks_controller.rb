a51f674d4c322bc95cd44e1b45008b1b55ed753b
Paolo Zaccagnini <paozac@gmail.com>
2013-02-20 10:48:20 +0100

    Let's try to redirect the user with omniauth.origin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def rtl
-    active = Scenario.for_user(current_or_guest_user).recent.first
     @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
     sign_in @user
 
-    if active
-      redirect_to scene_scenario_path(scene_id: active.scene_id, id: active.id)
-    else
-      redirect_to root_path, notice: "succesfully signed in!"
-    end
+    redirect_to request.env['omniauth.origin'] || root_path
   rescue ActiveRecord::RecordInvalid
     raise request.env['omniauth.auth']
   end
