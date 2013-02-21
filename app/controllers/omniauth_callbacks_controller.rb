1951149af1f12d66d1258cd44a03426beb79d6d2
Paolo Zaccagnini <paozac@gmail.com>
2013-02-21 10:16:21 +0100

    Change current scenario ownership after RTL login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
     sign_in @user
 
-    redirect_to '/scenes/1'
+    if referrer = request.env['omniauth.origin']
+      # Let's change the scenario ownership. Ugly stuff
+      session_id = referrer.split('/').last.to_i
+      scenario = Scenario.find_by_session_id(session_id)
+      if scenario
+        scenario.user_id = @user.id
+        scenario.save
+      end
+      redirect_to referrer
+    else
+      redirect_to '/scenes/1'
+    end
   rescue ActiveRecord::RecordInvalid
     raise request.env['omniauth.auth']
   end
