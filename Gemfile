9ae5a476c4ad61150aa0799e332b6c525ccfc304
Paolo Zaccagnini <paozac@gmail.com>
2013-02-28 17:15:51 +0100

    Trash: RTL login
    Use the rtl-auth branch

--- Gemfile
+++ Gemfile
@@ -28,7 +28,6 @@ gem 'rest-client'
 gem 'devise'
 gem 'omniauth-facebook'
 gem 'omniauth-twitter'
-gem 'omniauth-openid'
 
 gem 'jquery-rails', '~> 2.1.4'
 gem 'jquery-ui-rails'
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,25 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def rtl
-    @user = User.find_or_create_with_rtl!(request.env['omniauth.auth'])
-    sign_in @user
-
-    if referrer = request.env['omniauth.origin']
-      # Let's change the scenario ownership. Ugly stuff
-      session_id = referrer.split('/').last.to_i
-      scenario = Scenario.find_by_session_id(session_id)
-      if scenario
-        scenario.user_id = @user.id
-        scenario.save
-      end
-      redirect_to referrer
-    else
-      redirect_to '/scenes/1'
-    end
-  rescue ActiveRecord::RecordInvalid
-    raise request.env['omniauth.auth']
-  end
-
   def facebook
     @user = User.find_or_create_with_facebook!(request.env['omniauth.auth'])
 
