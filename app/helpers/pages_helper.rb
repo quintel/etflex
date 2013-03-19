baa36870c068680d0912ccf5dbbb4235f53da76a
Andre Medeiros <me@andremedeiros.info>
2013-03-19 11:33:53 +0100

    Make the images point to the default action.

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,11 @@
+module PagesHelper
+  def create_or_continue_playing_path(scene)
+    previous_attempt = scene.previous_attempt(current_or_guest_user)
+
+    if previous_attempt
+      scene_scenario_path(scene, previous_attempt)
+    else
+      scene_path(scene)
+    end
+  end
+end
