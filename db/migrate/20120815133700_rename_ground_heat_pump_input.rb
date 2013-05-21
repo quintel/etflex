38a47987443955ad9aafece2dae2f38bd11f4658
Chael Kruip <chael.kruip@quintel.com>
2013-05-21 16:57:05 +0200

    Changed all inputs-names. Part of etsource/212

--- db/migrate/20120815133700_rename_ground_heat_pump_input.rb
+++ db/migrate/20120815133700_rename_ground_heat_pump_input.rb
@@ -1,13 +1,13 @@
 class RenameGroundHeatPumpInput < ActiveRecord::Migration
   def up
-    input = Input.where(key: 'households_heating_heat_pump_ground_share').first!
+    input = Input.where(key: 'households_space_heater_heatpump_ground_water_electricity_share').first!
     input.key = 'households_space_heater_heatpump_ground_water_electricity'
     input.save!
   end
 
   def down
     input = Input.where(key: 'households_space_heater_heatpump_ground_water_electricity').first!
-    input.key = 'households_heating_heat_pump_ground_share'
+    input.key = 'households_space_heater_heatpump_ground_water_electricity_share'
     input.save!
   end
 end
