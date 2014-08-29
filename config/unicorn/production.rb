8a80a6b468549142d4c6b52e3d0dbba7955d44b0
Anthony Williams <hi@antw.io>
2014-08-29 17:38:43 +0100

    Update Capistrano to version 3

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -15,10 +15,10 @@ timeout 30
 
 # This is where we specify the socket. We will point the upstream Nginx module
 # to this socket later on
-listen "#{app_dir}/shared/pids/unicorn.sock", backlog: 64
+listen "#{app_dir}/shared/tmp/sockets/unicorn.sock", backlog: 64
 
 # File containing the Unicorn process ID.
-pid "#{app_dir}/shared/pids/unicorn.pid"
+pid "#{app_dir}/shared/tmp/pids/unicorn.pid"
 
 # Set the path of the log files inside the log folder of the testapp
 stderr_path "#{app_dir}/shared/log/unicorn.log"
@@ -37,7 +37,7 @@ before_fork do |server, worker|
   # around until it is explicitly killed (so that it can be used if the new
   # master fails to start). Since we got as far as starting a new worker, we
   # end the old process...
-  old_pid = "#{app_dir}/shared/pids/unicorn.pid.oldbin"
+  old_pid = "#{app_dir}/shared/tmp/pids/unicorn.pid.oldbin"
 
   if File.exists?(old_pid) && server.pid != old_pid
     begin
