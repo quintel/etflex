e115a32c2b6fefc8947186a173f821c11f9c7f3a
Anthony Williams <hi@antw.io>
2014-09-22 13:06:45 +0100

    Add maintain_test_schema! to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ Capybara.ignore_hidden_elements = false
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+ActiveRecord::Migration.maintain_test_schema!
+
 RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
