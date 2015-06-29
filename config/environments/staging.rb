46cbe3a9b80d3e01f81d2e307b7e0b4794d4d3ac
ThomasMAC <tuds_wilkinson@hotmail.com>
2015-06-29 15:12:03 +0200

    Remove 'Rails 4 style' dump_schema line in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,7 +82,4 @@ ETFlex::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
