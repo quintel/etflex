8276bb512453637430cb27dfb2f540309a08d372
ThomasMAC <tuds_wilkinson@hotmail.com>
2015-06-29 17:12:28 +0200

    Move symlink tmp/sockets -> sockets

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :linked_files,
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{
-  bin log pids tmp/cache tmp/sockets vendor/bundle
+  bin log pids tmp/cache sockets vendor/bundle
   public/system
 }
 
